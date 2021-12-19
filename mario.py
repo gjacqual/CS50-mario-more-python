4a90ac62752c0c7019795fcc6ebf104cbf3202f6
Max F. Gjacqual <89233376+gjacqual@users.noreply.github.com>
2021-12-19 18:41:02 +0300

    Add files via upload

---
+++ mario.py
@@ -0,0 +1,19 @@
+from cs50 import get_int
+# Ask height of pyromid
+while True:
+    height = get_int("Height: ")
+    if 0 < height < 9:
+        break
+# draw Pyromid
+for i in range(height):
+    spaces = height - 1
+    while spaces > i:
+        print(" ", end="")
+        spaces -= 1
+    for j in range(i + 1):
+        print("#", end="")
+    print("  ", end="")
+    # draw second half of pyromid
+    for k in range(i + 1):
+        print("#", end="")
+    print()
